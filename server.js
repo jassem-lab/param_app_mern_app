72ee827b0f41763a521675c178f611a561d91704
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2020-12-17 07:05:00 +0100

    auth update

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./database/db");
-
+const cors = require("cors");
+const morgan = require("morgan");
 // Connection to Database
 
 connectDB();
 
+// Middleware
 
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
 
 // Routes ( API )
-
-
 
 // Production
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });} 
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
+// Start Listening
 
 const port = process.env.PORT || 5000;
 
